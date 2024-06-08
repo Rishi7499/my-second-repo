ad695cf875d7fe2fec08806716ffbb9478e5a628
Rishi7499 <manoharchougule51@gmail.com>
2024-06-08 12:29:51 +0530

    new file

---
+++ helloword.py
@@ -0,0 +1,12 @@
+num1=int(input("enter 1sr no:"))
+num2=int(input("enter 2nd no:"))
+choice=int(input("enter choice:1-addition,2-substraction,3-mul,4-div:"))
+print(choice)
+if (choice==1):
+    print(num1+num2)
+if (choice==2):
+    print(num1-num2)
+if (choice==3):
+    print(num1*num2)
+if (choice==4):
+    print(num1/num2)
